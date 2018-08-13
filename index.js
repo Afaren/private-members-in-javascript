ded3ccb29d23c9e125992b7bda9dce770bdf3ddb
Chongfa <cfachen@thoughtworks.com>
2018-08-13 21:15:48 +0800

    afaren - add script

---
+++ index.js
@@ -0,0 +1,27 @@
+function Container(param) {
+
+    function dec() {
+        if (secret > 0) {
+            secret -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.member = param;
+    var secret = 3;
+    var that = this;
+
+    this.service = function () {
+        return dec() ? that.member : null;
+    };
+}
+
+const container = new Container("abc");
+
+console.log(container.member);      // 3
+console.log(container.service());   // 3
+console.log(container.service());   // 3
+console.log(container.service());   // 3
+console.log(container.service());   // null
